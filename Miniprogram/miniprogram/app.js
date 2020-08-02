046ac34ea87aee0b30f31df80b7de340e90472d8
wkx <1292568820@qq.com>
2020-08-02 20:42:08 +0800

    首页搜索，轮播图，列表布局

--- Miniprogram/miniprogram/app.js
+++ Miniprogram/miniprogram/app.js
@@ -6,7 +6,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-         env: 'richwoman-env-iw8pg',
+        env: 'richwoman-env-iw8pg',
         traceUser: true,
       })
     }
--- Miniprogram/miniprogram/pages/index/index.js
+++ Miniprogram/miniprogram/pages/index/index.js
@@ -2,6 +2,42 @@
 const app = getApp()
 
 Page({
-  
+  data: {
+    active: 0,
+    icon: {
+      classify:'cloud://richwoman-env-iw8pg.7269-richwoman-env-iw8pg-1302762108/tabbar/tab-classify.svg',
+      gift: 'cloud://richwoman-env-iw8pg.7269-richwoman-env-iw8pg-1302762108/tabbar/tab-gift.svg',
+      aclassify:'cloud://richwoman-env-iw8pg.7269-richwoman-env-iw8pg-1302762108/tabbar/tab-ac-classify.svg',
+      agift:'cloud://richwoman-env-iw8pg.7269-richwoman-env-iw8pg-1302762108/tabbar/tab-ac-gift.svg'
+    },
+    
+    swiperImg:[
+      'cloud://richwoman-env-iw8pg.7269-richwoman-env-iw8pg-1302762108/swiper/pic1.jpeg',
+      'cloud://richwoman-env-iw8pg.7269-richwoman-env-iw8pg-1302762108/swiper/pic2.jpeg'
+    ],
+    swiperIdx: 0,
+    list:[
+      {
+        "url": "/pages/index/index",
+      },
+      {
+        "url": "/pages/classify/classify",
+      }
+    ]
+  },
+  //底部标签栏
+  onChange(event) {
+    // event.detail 的值为当前选中项的索引
+    this.setData({ active: event.detail });
+    wx.switchTab({
+      url: this.data.list[event.detail].url
+    });
+  },
+  // 轮播图
+  bindchange(e) {
+    this.setData({
+      swiperIdx: e.detail.current
+    })
+  },
 
 })
